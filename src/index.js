3c45bf114a5e8285eec4f0611cae717af6501e4e
Sarthak12903 <sarthak12903@gmail.com>
2025-02-01 12:00:40 +0530

    Connect to mongo atlas

--- src/index.js
+++ src/index.js
@@ -2,10 +2,22 @@ import mongoose from "mongoose";
 // require('dotenv').config({path:'./env'})
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 dotenv.config({
   path: "../env",
 });
-connectDB();
+connectDB()
+  .then(() => {
+    app.on("error", (error) => {
+      console.log("ERROR : ", error);
+    });
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server Is Runnning On ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log(`MongoDb connection failed !!! ${err}`);
+  });
 
 /*
 import express from "express";
