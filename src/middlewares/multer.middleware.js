a34896ab0b189f8ae1489ca84887fa966e24e8c0
Sarthak12903 <sarthak12903@gmail.com>
2025-02-06 10:49:10 +0530

    middleware added

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -10,3 +10,24 @@ const storage = multer.diskStorage({
 });
 
 export const upload = multer({ storage: storage });
+
+// URL = uniform resource locator
+// URI =  uniform resource indentifier
+// URN = uniform resource name
+
+/*
+What are HTTP headers: metdata ---> key-value sent along with request and response
+
+
+
+request headers --> from client
+response headers --> from server
+representation headers --> encoding/compression
+payload headers --> data
+
+
+
+
+HTTP methods:
+GET, HEAD , OPTIONS, TRACE, DELETE, PUT, POST, PATCh
+*/
