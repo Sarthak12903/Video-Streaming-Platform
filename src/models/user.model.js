48bf25df7e40396d5bb3e9d107972a5ee373eafa
Sarthak12903 <sarthak12903@gmail.com>
2025-02-08 12:51:22 +0530

    add register controller and bug fixes

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -50,7 +50,7 @@ const userSchema = new Schema(
 
 userSchema.pre("save", async function (next) {
   if (!this.isModified("password")) return next();
-  this.password = bcrypt.hash(this.password, 10);
+  this.password = await bcrypt.hash(this.password, 10);
   next();
 }); // Pre hook to perform hashing before storing it into database
 
