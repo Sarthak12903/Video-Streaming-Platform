f1d7a490f6c8f674424905a2af6495836defb527
Sarthak12903 <sarthak12903@gmail.com>
2025-02-09 13:33:05 +0530

    fixed all register controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -15,7 +15,7 @@ const registerUser = asyncHandler(async (req, res) => {
   // return response
 
   const { fullName, email, username, password } = req.body;
-  console.log(fullName, username, email, password);
+  //   console.log(fullName, username, email, password);
 
   if (
     [fullName, email, username, password].some((field) => field?.trim() === "")
@@ -23,16 +23,24 @@ const registerUser = asyncHandler(async (req, res) => {
     throw new ApiError(400, "All Fields are required!!!");
   }
 
-  const existedUser = User.findOne({
+  const existedUser = await User.findOne({
     $or: [{ username }, { email }],
   });
   if (existedUser) {
     throw new ApiError(409, "User with email or username already exists");
   }
   const avatarLocalPath = req.files?.avatar[0]?.path;
-  console.log(req.files);
-  const coverImageLocalPath = req.files?.coverImage[0]?.path;
+  //    
+  //   const coverImageLocalPath = req.files?.coverImage[0]?.path;
 
+  let coverImageLocalPath;
+  if (
+    req.files &&
+    Array.isArray(req.files.coverImage) &&
+    req.files.coverImage.length > 0
+  ) {
+    coverImageLocalPath = req.files.coverImage[0].path;
+  }
   if (!avatarLocalPath) {
     throw new ApiError(400, "Avatar file is required!!");
   }
@@ -40,7 +48,7 @@ const registerUser = asyncHandler(async (req, res) => {
   const avatar = await uploadCloudinary(avatarLocalPath);
   const coverImage = await uploadCloudinary(coverImageLocalPath);
   if (!avatar) {
-    throw new ApiError(400, "Avatar file is required!!");
+    throw new ApiError(400, "Avatar file is required!!2");
   }
 
   const user = await User.create({
@@ -52,11 +60,9 @@ const registerUser = asyncHandler(async (req, res) => {
     username: username.toLowerCase(),
   });
 
-  console.log(user);
   const createdUser = await User.findById(user._id).select(
     "-password -refreshToken"
   );
-  console.log(createdUser);
 
   if (!createdUser) {
     throw new ApiError(
@@ -67,7 +73,7 @@ const registerUser = asyncHandler(async (req, res) => {
 
   return res
     .status(201)
-    .json(new ApiResponse(200, createdUse, "User registered successfully"));
+    .json(new ApiResponse(200, createdUser, "User registered successfully"));
 });
 
 export { registerUser };
--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -10,13 +10,15 @@ cloudinary.config({
 
 const uploadCloudinary = async (localFilePath) => {
   try {
+    console.log(localFilePath);
     if (!localFilePath) return null;
-    const uploadResult = await cloudinary.uploader.upload(`localFilePath`, {
+    const uploadResult = await cloudinary.uploader.upload(`${localFilePath}`, {
       resource_type: "auto",
     });
 
-    // file has been uploaded successfully
-    console.log("File is uploaded on cloudinary", uploadResult.url);
+    // // file has been uploaded successfully
+    // console.log("File is uploaded on cloudinary", uploadResult.url);
+    fs.unlinkSync(localFilePath);
     return uploadResult;
   } catch (error) {
     // ab toh file server per hein per upload nahi hua so we have to remove that file from server to avoid malicious file
